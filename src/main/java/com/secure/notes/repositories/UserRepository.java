b832b6ae1af44692f941933bc3def742e88efdfa
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2024-12-07 16:02:51 +0530

    Setting the Signup Endpoint

--- src/main/java/com/secure/notes/repositories/UserRepository.java
+++ src/main/java/com/secure/notes/repositories/UserRepository.java
@@ -10,6 +10,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String username);
 
+    Boolean existsByEmail(String email);
+
     Boolean existsByUserName(String userName);
 }
 
